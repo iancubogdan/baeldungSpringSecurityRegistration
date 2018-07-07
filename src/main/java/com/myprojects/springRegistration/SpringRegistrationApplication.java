4a7af41c1d3eb8c11ed3c9101c73a78a51d51f0d
Bogdan Iancu <iancubogdan91@yahoo.com>
2018-07-07 20:33:36 +0300

    first commit

---
+++ src/main/java/com/myprojects/springRegistration/SpringRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.springRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegistrationApplication.class, args);
+	}
+}
